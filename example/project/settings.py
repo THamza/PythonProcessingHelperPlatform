dcd8ebafde5333e51a1fb7895aab7c4d06e7f12a
THamza <touhs.hamza@gmail.com>
2022-08-04 21:45:58 +0100

    another attempt to push to hjeroku

--- example/project/settings.py
+++ example/project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's917$+d8re=1@0sg39l985abmhlen3mo(o7=xy4v86#ka2pdsa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 #ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['*', ]
